deb86f32872f9ba7676612a9902895af1e0a82b8
Onwe Michael <elonwem@gmail.com>
2024-10-24 09:49:35 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    rollupOptions: {
+      // other options
+    },
+  },
+  server: {
+    historyApiFallback: true, // This ensures all routes fall back to index.html
+  },
+});
